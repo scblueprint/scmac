d46d8c70b3771431ea9bd9f6ce23e0e22a546c25
Anirudh Krishna <kris.anirudh@gmail.com>
2024-02-22 20:55:27 -0800

    fixed app.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Event from './pages/Events'
-import IndividualEvent from './pages/IndividualEvent'
 
 export default function App() {
   return (
-    <IndividualEvent />
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
