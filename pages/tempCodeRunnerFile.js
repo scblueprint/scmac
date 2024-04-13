21ba899750103f25b306de09a7cb0062333dab34
Vanessa Huynh <vanessatvh@Vanessas-MacBook-Air-2.local>
2024-04-12 18:25:46 -0700

    Complete Waiver

--- pages/tempCodeRunnerFile.js
+++ pages/tempCodeRunnerFile.js
@@ -1,9 +1,1 @@
-      <View style = {styles.rectangle}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={true}>
-          <Image
-            source={require('../assets/SCMAC-Waiver.png')}
-            style={styles.image}
-            resizeMode="contain"
-          />
-         </ScrollView>
-      </View>
+          onChangeText={setVolunteerDate}
