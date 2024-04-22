80412baf2a7e07287f4b3fa22a5086ea761367e3
Anirudh Krishna <kris.anirudh@gmail.com>
2024-04-22 00:43:23 -0700

    notis

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { app, firebaseMessaging } from './firebaseConfig';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -15,11 +16,65 @@ import CreateEventScreen from './pages/admin/createEvent';
 import EditEventScreen from './pages/admin/editEvent';
 import AdminEvents from './pages/admin/adminEvent';
 import AdminEventDetailScreen from './pages/admin/adminIndividualEvent';
-
+import React,  { useState, useEffect, useCallback } from 'react';
+import { Alert } from 'react-native';
+import messaging from '@react-native-firebase/messaging';
 const Stack = createStackNavigator();
-
+console.log("bro..")
 export default function App() {
   // persistence: getReactNativePersistence(ReactNativeAsyncStorage) //async-storage in Waiver page
+  const requestUserPermission = async () => {
+    console.log("HIHIHI")
+    const authStatus = await firebaseMessaging.requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  
+    if (enabled) {
+      console.log('Authorization status:', authStatus);
+    }
+
+  }
+  useEffect(()=>{
+    console.log('App initialized checking in App.js:', app.name); // This should log '[DEFAULT]'
+    if(app){
+    console.log("in use effect")
+    if(requestUserPermission){
+      firebaseMessaging.getToken().then(token=>{
+        console.log(token);
+      })
+    }
+    else{
+      console.log("Failed Token Status", authStatus)
+    }
+
+    firebaseMessaging.getInitialNotification().then(remoteMessage=>{
+      if(remoteMessage){
+        console.log('Notification caused app to open from quit state:',
+        remoteMessage.notification,
+      )
+      }
+    })
+    firebaseMessaging.onNotificationOpenedApp(async (remoteMessage)=>{
+      console.log('Notification caused app to open from background state:',
+      remoteMessage.notification
+    )
+    })
+
+    firebaseMessaging.setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the bavckground!', remoteMessage);
+    });
+
+    const unsubscribe = firebaseMessaging.onMessage(async remoteMessage => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }
+  else{
+    console.log("FAILED")
+  }
+  },[])
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName='Login'>
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,10 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/auth";
 import { ReactNativeAsyncStorage } from '@react-native-async-storage/async-storage';
 import { getStorage } from "firebase/storage";
+import messaging from '@react-native-firebase/messaging';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyDkqrT3nByTUhGITQQscLKhMpYstS8U2cw",
@@ -14,10 +16,36 @@ const firebaseConfig = {
   measurementId: "G-WX6CMZNQB3"
 };
 
-const app = initializeApp(firebaseConfig);
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+console.log('Firebase initializedd:', app.name);
+
 const db = getFirestore(app);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
-});
+  })
+console.log("Firebase Auth Initialized: ", auth);
+const firebaseMessaging = messaging();
+
+// Optionally, you may want to request user permission for notifications
+// This is typically done at the time of app launch or during the appropriate user flow
+async function requestUserPermission() {
+  const authorizationStatus = await firebaseMessaging.requestPermission({
+    provisional: false,
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: true,
+    criticalAlert: false,
+    sound: true,
+  });
+
+  if (authorizationStatus === messaging.AuthorizationStatus.AUTHORIZED || authorizationStatus === messaging.AuthorizationStatus.PROVISIONAL) {
+    console.log('Authorization status:', authorizationStatus);
+  }
+}
+
+// You may want to call this function in your app's component lifecycle
+requestUserPermission();
 const storage = getStorage(app);
-module.exports = { db, auth, storage };
+
+module.exports = { app, db, auth, storage, firebaseMessaging };
