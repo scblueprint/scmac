04dd5f7f35e59e51b107d5092030511df08782d4
Pari <pdar@ucsc.edu>
2024-05-07 10:44:59 -0700

    events filter UI working

--- pages/events.js
+++ pages/events.js
@@ -5,6 +5,8 @@ import NavBar from '../components/NavBar.js'
 import { collection, getDocs, query } from 'firebase/firestore';
 import { db } from '../firebaseConfig';
 import { useFocusEffect } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const EventItem = ({ item, nav }) => (
   <TouchableOpacity key={item.id} style={styles.itemContainer} onPress={()=>nav.navigate("IndividualEvent", {item: item})}>
@@ -18,8 +20,16 @@ const EventItem = ({ item, nav }) => (
 
 );
 
+
 export default function Events({navigation}) {
   const [events, setEvents] = useState([]);
+  const [filtercolor, setFiltercolor] = useState("#F1F1F2");
+  const [filtercolor1, setFiltercolor1] = useState("#F1F1F2");
+  const [filtercolor2, setFiltercolor2] = useState("#F1F1F2");
+  const [filtercolor3, setFiltercolor3] = useState("#F1F1F2");
+
+
+
   useFocusEffect(useCallback( () => {
     async function fetchData() {
       const arr = [];
@@ -36,6 +46,37 @@ export default function Events({navigation}) {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Events</Text>
+      <View style={styles.filter}>
+      <Ionicons name="filter-outline" size={30} color="black"/>
+      <TouchableOpacity style={{backgroundColor:filtercolor,
+    borderRadius: 10,
+    fontSize:15,
+    padding:10,}} onPress={()=>{if (filtercolor=="#A16AA4") {setFiltercolor("#F1F1F2")} else { setFiltercolor("#A16AA4")}}}>
+        <Text>All</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{backgroundColor:filtercolor1,
+    borderRadius: 10,
+    fontSize:15,
+    padding:10,}} onPress={()=>{if (filtercolor1=="#A16AA4") {setFiltercolor1("#F1F1F2")} else { setFiltercolor1("#A16AA4")}}}>
+        <Text>Ceramics</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{backgroundColor:filtercolor2,
+    borderRadius: 10,
+    fontSize:15,
+    padding:10,}} onPress={()=>{if (filtercolor2=="#A16AA4") {setFiltercolor2("#F1F1F2")} else { setFiltercolor2("#A16AA4")}}}>
+        <Text>Shows</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{backgroundColor:filtercolor3,
+    borderRadius: 10,
+    fontSize:15,
+    padding:10,}} onPress={()=>{if (filtercolor3=="#A16AA4") {setFiltercolor3("#F1F1F2")} else { setFiltercolor3("#A16AA4")}}}>
+        <Text>Art Gallery</Text>
+      </TouchableOpacity>
+      </View>
+
       <FlatList
         data={events}
         renderItem={({ item }) => (
@@ -89,4 +130,20 @@ const styles = StyleSheet.create({
     fontSize: 21,
     color: '#000000',
   },
+  filter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    //backgroundColor: 'white',
+    paddingTop: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginHorizontal: 3,
+    marginTop: 3,
+  },
+  filterButton: {
+    borderRadius: 10,
+    fontSize: 18,
+    padding: 15,
+  }
 });
